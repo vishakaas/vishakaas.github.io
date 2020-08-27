ae06037aee374e2109c0235a27ff5d29750069f8
Vishakaa Satheeshkumar <vsatheeshkumar@ryerson.ca>
2020-08-27 13:44:14 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,47 @@
+console.log('It Works')
+
+$(document).ready(function() {
+    $('.submit').click(function (event) {
+        console.log('Clicked Button')
+
+        var name = $('.name').val()
+        var email = $('.email').val()
+        var subject = $('.subject').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+
+        if(name.length > 2) {
+            statusElm.append('')
+        }
+        else {
+            event.preventDefault()
+            statusElm.append('<div>Name is not valid.</div>')
+        }
+
+        if(email.length > 5 && email.includes('@') && email.includes('.')) {
+            statusElm.append('')
+        }
+        else {
+            event.preventDefault()
+            statusElm.append('<div>Email is not valid.</div>')
+        }
+
+        if(subject.length >= 10) {
+            statusElm.append('')
+        }
+        else {
+            event.preventDefault()
+            statusElm.append('<div>Subject is not valid.</div>')
+        }
+
+        if(message.length >= 10) {
+            statusElm.append('')
+        }
+        else {
+            event.preventDefault()
+            statusElm.append('<div>Message is not valid</div>')
+        }
+        
+    })
+})
